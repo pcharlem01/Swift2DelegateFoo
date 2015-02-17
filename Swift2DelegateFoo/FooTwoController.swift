ec5dee4658961588231f0dc8464f1e371844d9f7
Philip <pcharlem01@gmail.com>
2015-02-16 22:00:07 -0500

    Added FooTwoController

---
+++ Swift2DelegateFoo/FooTwoController.swift
@@ -0,0 +1,35 @@
+//
+//  FooTwoController.swift
+//  Swift2DelegateFoo
+//
+//  Created by Philip Charlemagne on 2/16/15.
+//  Copyright (c) 2015 Fab Cab LLC. All rights reserved.
+//
+
+import UIKit
+
+class FooTwoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
