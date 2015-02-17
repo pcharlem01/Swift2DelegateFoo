18e792808510e64748cda1b38efbc0e8ffdc2e14
Philip <pcharlem01@gmail.com>
2015-02-16 21:39:53 -0500

    Initial Commit

---
+++ Swift2DelegateFoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift2DelegateFoo
+//
+//  Created by Philip Charlemagne on 2/16/15.
+//  Copyright (c) 2015 Fab Cab LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
